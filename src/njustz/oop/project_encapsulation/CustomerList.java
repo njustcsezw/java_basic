3b3a41046f00d51f8f65b7cf7d033f3606b324f7
njustcsezw <2990629037@qq.com>
2020-03-02 15:26:12 +0800

    update Customer

---
+++ src/njustz/oop/project_encapsulation/CustomerList.java
@@ -0,0 +1,16 @@
+package njustz.oop.project_encapsulation;
+
+/**
+ * @author njustz
+ * @date 2020/3/1
+ */
+public class CustomerList {
+
+    private Customer[] customers;
+    private int total = 0;
+
+    public CustomerList(){
+        
+    }
+
+}
---
+++ src/njustz/oop/project_encapsulation/CustomerView.java
@@ -0,0 +1,8 @@
+package njustz.oop.project_encapsulation;
+
+/**
+ * @author njustz
+ * @date 2020/3/1
+ */
+public class CustomerView {
+}
