8e0454cdeb7ddcfe24dc6a48d82e5a4abcb3ae2d
njustcsezw <2990629037@qq.com>
2020-03-03 13:20:22 +0800

    update CustomerView

--- src/njustz/oop/project_encapsulation/CustomerList.java
+++ src/njustz/oop/project_encapsulation/CustomerList.java
@@ -7,10 +7,57 @@ package njustz.oop.project_encapsulation;
 public class CustomerList {
 
     private Customer[] customers;
-    private int total = 0;
+    private int total;
 
-    public CustomerList(){
-        
+    public CustomerList(int totalCustomer){
+        this.customers = new Customer[totalCustomer];
+    }
+
+    public boolean addCustomer(Customer customer){
+        if(total >= customers.length){
+            return false;
+        }
+        customers[total++] = customer;
+        return true;
+    }
+
+    public boolean replaceCustomer(int index, Customer customer){
+        if(index < 0 || index >= total){
+            return false;
+        }
+        customers[index] = customer;
+        return true;
+    }
+
+    public boolean deleteCustomer(int index){
+        if(index < 0 || index >= total){
+            return false;
+        }
+        for(int i = index; i < total-1; i++){
+            customers[i] = customers[i + 1];
+        }
+        customers[total-1] = null;
+        total -- ;
+        return true;
+    }
+
+    public Customer[] getAllCustomers(){
+        Customer[] customerLists = new  Customer[total];
+        for(int i = 0; i < total; i++){
+            customerLists[i] = customers[i];
+        }
+        return customerLists;
+    }
+
+    public Customer getCustomer(int index){
+        if(index < 0 || index >= total){
+            return null;
+        }
+        return customers[index];
+    }
+
+    public int getTotal(){
+        return total;
     }
 
 }
--- src/njustz/oop/project_encapsulation/CustomerView.java
+++ src/njustz/oop/project_encapsulation/CustomerView.java
@@ -5,4 +5,71 @@ package njustz.oop.project_encapsulation;
  * @date 2020/3/1
  */
 public class CustomerView {
+
+    private CustomerList customerList = new CustomerList(10);
+
+    public void enterMainMenu(){
+
+        boolean isFlag = true;
+        while (isFlag){
+            System.out.println("\n---------------客户信息软件---------------");
+            System.out.println("                1添加客户");
+            System.out.println("                2修改客户");
+            System.out.println("                3删除客户");
+            System.out.println("                4客户列表");
+            System.out.println("                5退    出\n");
+            System.out.println("               请选择(1-5): ");
+
+            char menu = CMUtility.readMenuSelection();
+            switch (menu){
+                case '1':
+                    addNewCustomer();
+                    break;
+                case '2':
+                    modifyCustomer();
+                    break;
+                case '3':
+                    deleteCustomer();
+                    break;
+                case '4':
+                    listAllCustomer();
+                    break;
+                case '5':
+                    System.out.println("是否确定退出(Y/N): ");
+                    char isExit = CMUtility.readConfirmSelection();
+                    if (isExit == 'Y'){
+                        isFlag = false;
+                    }
+
+            }
+
+
+        }
+
+
+    }
+
+    private void addNewCustomer(){
+        System.out.println("添加客户");
+    }
+
+    private void modifyCustomer(){
+        System.out.println("修改客户");
+
+    }
+
+    private void deleteCustomer(){
+        System.out.println("删除客户");
+    }
+
+    private void listAllCustomer(){
+        System.out.println("客户列表");
+
+    }
+
+    public static void main(String[] args) {
+        CustomerView customerView = new CustomerView();
+        customerView.enterMainMenu();
+    }
+
 }
