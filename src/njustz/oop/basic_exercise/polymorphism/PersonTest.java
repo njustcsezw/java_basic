b18ad8766b2a56a74a9f437c2a44fffd809238e4
njustcsezw <2990629037@qq.com>
2020-03-04 22:38:04 +0800

    update polymorphism

--- src/njustz/oop/basic_exercise/polymorphism/PersonTest.java
+++ src/njustz/oop/basic_exercise/polymorphism/PersonTest.java
@@ -14,12 +14,15 @@ public class PersonTest {
         Man man = new Man();
         man.eat();
         man.walk();
+        man.earn();
         System.out.println("*************");
 
         //多态性：父类的引用指向子类对象
+        //编译期看左边，运行期看右边
         Person p2 = new Man();
         p2.eat();
         p2.walk();
+        // p2.earn(); 找不到earn方法，报错
         System.out.println("*************");
 
     }
