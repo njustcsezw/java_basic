8fef93e86b30f179e28d5618381a1ad3e7f7ebd7
njustcsezw <2990629037@qq.com>
2020-03-05 20:31:10 +0800

    update TemplateTest

---
+++ src/njustz/pattern_design/TemplateTest.java
@@ -0,0 +1,48 @@
+package njustz.pattern_design;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+/**
+ * @author njustz
+ * @date 2020/3/5
+ */
+public class TemplateTest {
+    public static void main(String[] args) {
+        Template template = new SubTemplate();
+        template.spendTime();
+    }
+
+}
+
+abstract class Template{
+
+    public void spendTime(){
+        long start = System.currentTimeMillis();
+        code();
+        long end = System.currentTimeMillis();
+        System.out.println("the cost of time is: " + (end-start));
+    }
+
+    public abstract void code();
+
+}
+
+class SubTemplate extends Template{
+
+    @Override
+    public void code() {
+        //output prime between 2~1000
+        for(int i = 2; i < 1000; i++){
+            boolean isFlag = true;
+            for(int j = 2; j <= Math.sqrt(i); j++){
+                if(i % j == 0){
+                    isFlag = false;
+                    break;
+                }
+            }
+            if(isFlag){
+                System.out.println(i);
+            }
+        }
+    }
+}
