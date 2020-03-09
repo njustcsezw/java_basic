7fc4bd622b957b2c988875c3c07ec65b06f2f145
njustcsezw <2990629037@qq.com>
2020-03-09 22:06:00 +0800

    update String

---
+++ src/njustz/basiclib/string/StringTest.java
@@ -0,0 +1,28 @@
+package njustz.basiclib.string;
+
+/**
+ * @author njustz
+ * @date 2020/3/8
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String s1 = "helloworld";
+        System.out.println(s1.length());
+        System.out.println(s1.charAt(5));
+        System.out.println(s1.isEmpty());
+        String s2 = s1.toUpperCase();
+        System.out.println("s2: " + s2);
+        System.out.println("s1: " + s1);
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+        String s3 = " he ll world  ";
+        String s4 = s3.trim();
+        System.out.println("s3: " + s3 + "*");
+        System.out.println("s4: " + s4 + "*");
+
+        String s5 = "111222333";
+        String s6 = s5.substring(0, 7);
+        System.out.println(s6);
+
+    }
+}
