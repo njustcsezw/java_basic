927386de0b3f1a35d3b57ce50bd156d6bf2152f7
njustcsezw <2990629037@qq.com>
2020-03-10 15:21:09 +0800

    update String

--- src/njustz/basiclib/string/StringTest2.java
+++ src/njustz/basiclib/string/StringTest2.java
@@ -2,6 +2,7 @@ package njustz.basiclib.string;
 
 /**
  * StringBuffer与StringBuilder的使用
+ * 前者线程安全效率低，后者线程不安全效率高
  * @author njustz
  * @date 2020/3/10
  */
@@ -13,11 +14,17 @@ public class StringTest2 {
         //char[] value = new char[16];
         stringBuffer1.append("abc");
         System.out.println(stringBuffer1);
-
         StringBuffer stringBuffer2 = new StringBuffer("abc");
         //char[] value = new char["abc".length + 16]
         System.out.println("stringBuffer1 == StringBuffer2?" + (stringBuffer1 == stringBuffer2));
 
+
+        System.out.println("***StringBuilder***");
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("abc");
+        System.out.println(stringBuffer);
+        stringBuffer.delete(1,2);
+        System.out.println(stringBuffer);
 
     }
 }
