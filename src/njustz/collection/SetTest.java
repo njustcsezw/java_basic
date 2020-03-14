fda0fd059d547f3cec9994cafda79de06eb0ddb0
njustcsezw <2990629037@qq.com>
2020-03-14 15:09:48 +0800

    update Set

---
+++ src/njustz/collection/SetTest.java
@@ -0,0 +1,29 @@
+package njustz.collection;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @author njustz
+ * @date 2020/3/14
+ */
+public class SetTest {
+    public static void main(String[] args) {
+        Set set = new HashSet();
+        set.add(123);
+        set.add("AA");
+        set.add("BB");
+        set.add("CC");
+        set.add(456);
+        System.out.println(set);
+
+        Set set1 = new LinkedHashSet();
+        set1.add(123);
+        set1.add("AA");
+        set1.add("BB");
+        set1.add("CC");
+        set1.add(456);
+        System.out.println(set1);
+    }
+}
