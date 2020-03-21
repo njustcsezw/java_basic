32353ab88e683bff491478e2746b96afd2b6a2ee
njustcsezw <2990629037@qq.com>
2020-03-21 14:22:12 +0800

    update Map

---
+++ src/njustz/collection/PropertiesTest.java
@@ -0,0 +1,27 @@
+package njustz.collection;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+/**
+ * @author njustz
+ * @date 2020/3/21
+ */
+public class PropertiesTest {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream("E:\\workCrazyJava\\src\\njustz\\collection\\jdbc.properties");
+            properties.load(fileInputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String name = properties.getProperty("name");
+        System.out.println(name);
+        String password = properties.getProperty("password");
+        System.out.println(password);
+    }
+}
