53c8e5ba3254110efc26256f841ce9323c994561
njustcsezw <2990629037@qq.com>
2020-03-23 13:55:14 +0800

    update BufferedTest

---
+++ src/njustz/basiclib/IO/BufferedTest.java
@@ -0,0 +1,12 @@
+package njustz.basiclib.IO;
+
+/**
+ * 缓冲流Buffered~ 提高流的读取、写入速度
+ * @author njustz
+ * @date 2020/3/23
+ */
+public class BufferedTest {
+    public static void main(String[] args) {
+
+    }
+}
