ea7740bfaade5f5e13115dc4efea5d95201f750b
njustcsezw <2990629037@qq.com>
2020-03-25 14:43:07 +0800

    update SocketProgramming

--- src/njustz/basiclib/Internet/ClientFileTest.java
+++ src/njustz/basiclib/Internet/ClientFileTest.java
@@ -11,6 +11,8 @@ import java.net.Socket;
 public class ClientFileTest {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 8899);
+
+        //发送文件到服务器端
         OutputStream os = socket.getOutputStream();
         FileInputStream fileInputStream = new FileInputStream(new File("E:\\workCrazyJava\\src\\njustz\\basiclib\\IO\\photo.jpg"));
         byte[] bytes = new byte[1024];
@@ -18,7 +20,20 @@ public class ClientFileTest {
         while ((len = fileInputStream.read(bytes)) != -1){
             os.write(bytes, 0 , len);
         }
+        socket.shutdownOutput();
 
+        //接收来自服务器端的消息
+        InputStream inputStream = socket.getInputStream();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] bytes1 = new byte[1024];
+        int len1;
+        while ((len1 = inputStream.read(bytes1)) != -1){
+            baos.write(bytes1, 0, len1);
+        }
+        System.out.println(baos.toString());
+
+        baos.close();
+        inputStream.close();
         fileInputStream.close();
         os.close();
         socket.close();
--- src/njustz/basiclib/Internet/ServerFileTest.java
+++ src/njustz/basiclib/Internet/ServerFileTest.java
@@ -1,9 +1,6 @@
 package njustz.basiclib.Internet;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,7 +13,7 @@ public class ServerFileTest {
         ServerSocket ss = null;
         Socket socket = null;
         InputStream is = null;
-        FileOutputStream fileOutputStream = new FileOutputStream(new File("E:\\workCrazyJava\\src\\njustz\\basiclib\\IO\\photoServer.jpg"));
+        FileOutputStream fileOutputStream = new FileOutputStream(new File("E:\\workCrazyJava\\src\\njustz\\basiclib\\IO\\photoServer1.jpg"));
         ss = new ServerSocket(8899);
         socket = ss.accept();
         is = socket.getInputStream();
@@ -25,7 +22,12 @@ public class ServerFileTest {
         while ((len = is.read(bytes)) != -1){
             fileOutputStream.write(bytes, 0, len);
         }
+
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("File has been accepted~".getBytes());
+
         fileOutputStream.close();
+        outputStream.close();
         is.close();
         socket.close();
         ss.close();
