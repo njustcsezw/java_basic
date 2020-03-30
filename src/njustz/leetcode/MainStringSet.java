4d5e3e12b0a4267f80676ee311ee5c9cb1298857
njustcsezw <2990629037@qq.com>
2020-03-30 14:05:00 +0800

    update leetcode

--- src/njustz/leetcode/MainStringSet.java
+++ src/njustz/leetcode/MainStringSet.java
@@ -1,5 +1,6 @@
 package njustz.leetcode;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 /**
@@ -13,7 +14,37 @@ import java.util.Scanner;
 public class MainStringSet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.next();
+        while (scanner.hasNext()){
+            String str = scanner.next();
+
+            String str1 = "";
+            for (int i = str.length()-1; i >= 0 ; i--) {
+                boolean isFlag = true;
+                for (int j = i-1; j >= 0; j--) {
+                    if(str.charAt(i) == str.charAt(j))
+                        isFlag = false;
+                }
+                if(isFlag){
+                    str1 += str.charAt(i);
+                }
+            }
+            String str2 = "";
+            for (int i = str1.length()-1; i >= 0; i--) {
+                str2 += str1.charAt(i);
+            }
+            System.out.println(str2);
+
+          /*  HashSet set = new HashSet();
+            String str3 = "";
+            for (int i = 0; i < str.length(); i++) {
+                if(set.add(str.charAt(i)))
+                    str3 += str.charAt(i);
+            }
+            System.out.println(str3);*/
+
+        }
 
     }
 }
+
+
