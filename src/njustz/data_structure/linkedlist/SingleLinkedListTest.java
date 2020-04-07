6349d1fb001a03829f44399885e29aa157870b26
njustcsezw <2990629037@qq.com>
2020-04-07 18:41:27 +0800

    update singlelinkedlist

--- src/njustz/data_structure/linkedlist/SingleLinkedListTest.java
+++ src/njustz/data_structure/linkedlist/SingleLinkedListTest.java
@@ -9,12 +9,23 @@ public class SingleLinkedListTest {
         HeroNode heroNode1 = new HeroNode(1,"sj","jsy");
         HeroNode heroNode2 = new HeroNode(2, "ljy", "yql");
         HeroNode heroNode3 = new HeroNode(3,"wy", "zdx");
+        HeroNode heroNode4 = new HeroNode(3,"wy11", "zdx11");
 
         SingleLinkedList singleLinkedList = new SingleLinkedList();
         singleLinkedList.add(heroNode1);
         singleLinkedList.add(heroNode2);
         singleLinkedList.add(heroNode3);
+        //singleLinkedList.addByOrder(heroNode3);
         singleLinkedList.showList();
+        //-----------------------------
+
+        SingleLinkedList singleLinkedList11 = new SingleLinkedList();
+        singleLinkedList11.addByOrder(heroNode1);
+        singleLinkedList11.addByOrder(heroNode3);
+        singleLinkedList11.addByOrder(heroNode2);
+        singleLinkedList11.showList();
+        singleLinkedList11.update(heroNode4);
+        singleLinkedList11.showList();
     }
 
     static class HeroNode{
@@ -52,6 +63,66 @@ public class SingleLinkedListTest {
                 temp = temp.next;
             }
             temp.next = heroNode;
+        }
+
+
+        //按照某一属性进行排序添加
+        public void addByOrder(HeroNode heroNode){
+            HeroNode temp = head;
+            boolean flag = false;
+            while (true){
+                if(temp.next == null){
+                    //temp.next = heroNode;
+                    break;
+                }
+                if(temp.next.no > heroNode.no){
+                    //heroNode.next = temp.next;
+                    //temp.next = heroNode;
+                    break;
+                }
+                if(temp.next.no == heroNode.no){
+                    flag =true;
+                    break;
+                }
+                temp = temp.next;
+            }
+
+            if(flag){
+                System.out.println("the heroNode is exist");
+                return;
+            }else {
+                heroNode.next = temp.next;
+                temp.next = heroNode;
+            }
+
+        }
+
+        public void update(HeroNode heroNode){
+            if(head.next == null){
+                System.out.println("The linkedlist is empty");
+                return;
+            }
+
+            HeroNode temp = head.next;
+            boolean flag = false;
+            while (true){
+                if(temp == null){
+                    break;
+                }
+                if(temp.no == heroNode.no){
+                    flag = true;
+                    break;
+                }
+                temp = temp.next;
+            }
+
+            if(flag){
+                temp.name = heroNode.name;
+                temp.nickName = heroNode.nickName;
+            }else {
+                System.out.println("not find");
+            }
+
         }
 
         public void showList(){
