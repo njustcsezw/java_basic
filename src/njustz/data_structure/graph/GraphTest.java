956d419eab63aeb78a0702ff04608d605250cf67
njustcsezw <2990629037@qq.com>
2020-04-10 15:33:35 +0800

    update graph

--- src/njustz/data_structure/graph/GraphTest.java
+++ src/njustz/data_structure/graph/GraphTest.java
@@ -2,11 +2,13 @@ package njustz.data_structure.graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * @author njustz
  * @date 2020/4/10
  */
+
 public class GraphTest {
     public static void main(String[] args) {
         int n = 5;
@@ -21,6 +23,8 @@ public class GraphTest {
         graph.insertEdge(1, 3, 1);
         graph.insertEdge(1, 4, 1);
         graph.showGraph();
+        //graph.dfsShow();
+        graph.bfsShow();
     }
 }
 
@@ -29,11 +33,82 @@ class Graph{
     private ArrayList<String> vertexList;
     private int[][] edges;
     private int numOfEdges;
+    private boolean[] isVisited;
 
     public Graph(int n){
         edges = new int[n][n];
         vertexList = new ArrayList<>(n);
         numOfEdges = 0;
+        isVisited = new boolean[n];
+    }
+
+    //得到第一个邻接节点的坐标
+    //不存在则返回-1
+    public int getFirstNeighbor(int index){
+        for (int i = 0; i < vertexList.size(); i++) {
+            if(edges[index][i] > 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getNextNeighbor(int v1, int v2){
+        for(int i = v2+1; i<vertexList.size(); i++){
+            if(edges[v1][i]>0)
+                return i;
+        }
+        return -1;
+    }
+
+    public void dfsShow(boolean[] isVisited, int i){
+        System.out.print(getValueByIndex(i)+ "->");
+        isVisited[i] = true;
+        int w = getFirstNeighbor(i);
+        while (w != -1){
+            if(!isVisited[w]){
+                dfsShow(isVisited, w);
+            }
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    public void bfsShow(boolean[] isVisited, int i){
+        int u;
+        int w;
+        LinkedList queue = new LinkedList();
+        System.out.print(getValueByIndex(i) + "->");
+        isVisited[i] = true;
+        queue.addLast(i);
+        while (!queue.isEmpty()){
+            u = (Integer)queue.removeFirst();
+            w = getFirstNeighbor(u);
+            while (w != -1){
+                if(!isVisited[w]){
+                    System.out.print(getValueByIndex(w)+"->");
+                    isVisited[w] = true;
+                    queue.addLast(w);
+                    //bfsShow(isVisited, w);
+                }
+                w = getNextNeighbor(u, w);
+            }
+        }
+    }
+
+    public void dfsShow(){
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if(!isVisited[i]){
+                dfsShow(isVisited, i);
+            }
+        }
+    }
+
+    public void bfsShow(){
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if(!isVisited[i]){
+                bfsShow(isVisited,i);
+            }
+        }
     }
 
     public void insertVertex(String vertex){
